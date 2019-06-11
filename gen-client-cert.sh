3f7995bef129c86bea24fa69d9e2d57df4c6b357
god4 <speclog@163.com>
2019-06-11 20:49:42 +0800

    删除VPN字样
    去掉或修改关于VPN的描述字符串

--- gen-client-cert.sh
+++ gen-client-cert.sh
@@ -12,4 +12,4 @@ signing_key
 tls_www_client
 _EOF_
 certtool --generate-certificate --load-privkey $USER-key.pem --load-ca-certificate $CA_DIR/ca-cert.pem --load-ca-privkey $CA_DIR/ca-key.pem --template user.tmpl --outfile $USER-cert.pem
-openssl pkcs12 -export -inkey $USER-key.pem -in $USER-cert.pem -name "$USER VPN Client Cert" -certfile $CA_DIR/ca-cert.pem -out $USER.p12
+openssl pkcs12 -export -inkey $USER-key.pem -in $USER-cert.pem -name "$USER RPS Client Cert" -certfile $CA_DIR/ca-cert.pem -out $USER.p12
